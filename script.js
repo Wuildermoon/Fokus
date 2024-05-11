d2010ff8939a311809a705ceb18a2d2a5b52cc2c
wuilderman2000@gmail.com <wuilderman2000@gmail.com>
2024-05-11 17:46:02 -0500

    se refactoriza el codigo javascript; se crea funcion 'changeContext(context)' la cual recibe como parametro el contexto de cada evento cuando se preciona un boton especifico; se añade estructura condicional swirch para cambiar el titulo de la pagina para cada contexto; se añade el codigo para llamar a la funcion 'changeContext' para cada evento

--- script.js
+++ script.js
@@ -2,15 +2,44 @@ const html = document.querySelector('html');
 const buttonEnfoque = document.querySelector(".app__card-button--enfoque");
 const buttonCorto = document.querySelector(".app__card-button--corto");
 const buttonLargo = document.querySelector(".app__card-button--largo");
+const banner = document.querySelector(".app__image");
+const title = document.querySelector(".app__title");
 
 buttonEnfoque.addEventListener("click", () => {
-    html.setAttribute("data-contexto", "enfoque");
+    changeContext("enfoque")
 })
 
 buttonCorto.addEventListener("click", () => {
-    html.setAttribute("data-contexto", "descanso-corto");
+    changeContext("descanso-corto")
 })
 
 buttonLargo.addEventListener("click", () => {
-    html.setAttribute("data-contexto", "descanso-largo");
+    changeContext("descanso-largo")
 })
+
+function changeContext(context){
+    html.setAttribute("data-contexto", context);
+    banner.setAttribute("src", `/imagenes/${context}.png`);
+    switch (context) {
+        case "enfoque":
+            title.innerHTML = `
+            Optimiza tu productividad,<br>
+                <strong class="app__title-strong">Sumérgete en lo que importa.</strong>
+            `
+            break;
+        case "descanso-corto":
+            title.innerHTML = `
+            ¿Qué tal tomar un respiro?,<br>
+                <strong class="app__title-strong">¡Haz una pausa corta.</strong>
+            `
+            break;
+        case "descanso-largo":
+            title.innerHTML = `
+            Hora de volver a la superficie,<br>
+                <strong class="app__title-strong">Haz una pausa larga.</strong>
+            `
+            break;
+        default:
+            break;
+    }
+}
