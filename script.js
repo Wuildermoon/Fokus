55f38c3f0df6b5d86fffd2f0d6a09202d532b25a
wuilderman2000@gmail.com <wuilderman2000@gmail.com>
2024-05-11 18:43:18 -0500

    se agrego un codigo para añadir la clase 'active' a los botones cuando se presionan; se agrego codigo a la funcion 'changeContext' para eliminar la clase 'active' de todos los botones que no se hayan presionado mediante un loop; se crea un nuevo elemento 'Audio' que se va a reproducir o pausar cuando ocurra un cambio en el input con el id 'alternar-musica', dandole la caracteristica de loop para que se reproduzca de nuevo cuando llegue al final de la duracion del audio

--- script.js
+++ script.js
@@ -4,22 +4,43 @@ const buttonCorto = document.querySelector(".app__card-button--corto");
 const buttonLargo = document.querySelector(".app__card-button--largo");
 const banner = document.querySelector(".app__image");
 const title = document.querySelector(".app__title");
+const buttons = document.querySelectorAll(".app__card-button");
+const inputPlayOrPauseMusic = document.querySelector("#alternar-musica");
+const music = new Audio('/sonidos/luna-rise-part-one.mp3');
+
+music.loop = true;
+
+inputPlayOrPauseMusic.addEventListener("change", () => {
+    if (music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
+})
 
 buttonEnfoque.addEventListener("click", () => {
     changeContext("enfoque")
+    buttonEnfoque.classList.add("active");
 })
 
 buttonCorto.addEventListener("click", () => {
     changeContext("descanso-corto")
+    buttonCorto.classList.add("active");
 })
 
 buttonLargo.addEventListener("click", () => {
     changeContext("descanso-largo")
+    buttonLargo.classList.add("active");
 })
 
 function changeContext(context){
+    buttons.forEach(function(context){
+        context.classList.remove("active");
+    });
+
     html.setAttribute("data-contexto", context);
     banner.setAttribute("src", `/imagenes/${context}.png`);
+
     switch (context) {
         case "enfoque":
             title.innerHTML = `
